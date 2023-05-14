880941b9f601ceb3627debf709d4e2a9abab6652
Joe Toledano <joseph.a.toledano@gmail.com>
2023-05-14 19:10:24 -0400

    removing the api key

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -5,7 +5,4 @@ export const OPBalancesEndpoint = 'https://api.openpool.co/wallet/balance/';
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
-export const OPENPOOL_API_KEY =
-  IS_PRODUCTION && process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? '31dda58a54e10751883afb9fb432cecc46be9786'
-    : '31dda58a54e10751883afb9fb432cecc46be9786';
+export const OPENPOOL_API_KEY = '';
